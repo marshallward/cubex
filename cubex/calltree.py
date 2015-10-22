9f6869727d4fecc040cbfcd5a2c09c73d3548a02
Marshall Ward <marshall.ward@gmail.com>
2015-10-22 14:14:35 +1100

    CallTree print function added

--- cubex/calltree.py
+++ cubex/calltree.py
@@ -1,19 +1,27 @@
 class CallTree(object):
 
-    def __init__(self, node, parent=None):
+    def __init__(self, node, cube, parent=None):
 
         self.call_id = int(node.get('id'))
-        self.region_id = int(node.get('calleeId'))
+        #self.region_id = int(node.get('calleeId'))
         self.metrics = {}
+
+        region_id = int(node.get('calleeId'))
+        self.region = cube.regions[region_id]
 
         self.parent = parent
         self.children = []
 
         for child_node in node.findall('cnode'):
-            child_tree = CallTree(child_node, self)
+            child_tree = CallTree(child_node, cube)
             self.children.append(child_tree)
 
     def update_index(self, index):
         index.extend(self.children)
         for child in self.children:
             child.update_index(index)
+
+    def print_tree(self, indent=''):
+        print(indent + '- ' + self.region.name)
+        for child in self.children:
+            child.print_tree(indent + '  ')
